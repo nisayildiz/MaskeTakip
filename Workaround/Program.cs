3221e549551252d5dcf186d9d386d93bda7ec936
nisayildiz <nisayildiz176@gmail.com>
2024-02-09 18:02:08 +0300

    kodlama.io C# Software Development Programming Camp

---
+++ Workaround/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+using System.Transactions;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.WriteLine();
+        Person person1 = new Person();
+        person1.FirstName = "Nisa";
+        person1.LastName = "Yıldız";
+        person1.DateOfBirthYear = 1998;
+        person1.NationalIdentity = 1;
+
+
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+        Console.ReadLine();
+    }
+
+
+
+}
+
+
+
+    
+
+
+
